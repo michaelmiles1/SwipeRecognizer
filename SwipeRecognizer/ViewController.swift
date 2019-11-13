7f0ebb69a4f3bb5e71ed196e190b8fae1a11e844
michaelmiles1 <michael.gage.miles@gmail.com>
2019-11-12 17:18:06 -0800

    Set up starter project.

--- SwipeRecognizer/ViewController.swift
+++ SwipeRecognizer/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var upLabel: UILabel!
+    @IBOutlet weak var downLabel: UILabel!
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
