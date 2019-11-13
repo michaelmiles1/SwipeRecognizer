99f654cda86ff8c21b08ef834d67ea164ab4ff42
michaelmiles1 <michael.gage.miles@gmail.com>
2019-11-12 17:58:05 -0800

    Added Reset button functionality.

--- SwipeRecognizer/ViewController.swift
+++ SwipeRecognizer/ViewController.swift
@@ -15,11 +15,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftLabel: UILabel!
     @IBOutlet weak var rightLabel: UILabel!
     
+    var upCount = 0
+    var downCount = 0
+    var leftCount = 0
+    var rightCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func resetCount(_ sender: Any) {
+        upCount = 0
+        upLabel.text = "UP: \(upCount)"
+        
+        downCount = 0
+        downLabel.text = "DOWN: \(downCount)"
+        
+        leftCount = 0
+        leftLabel.text = "LEFT: \(leftCount)"
+        
+        rightCount = 0
+        rightLabel.text = "RIGHT: \(rightCount)"
+    }
+    
 }
 
